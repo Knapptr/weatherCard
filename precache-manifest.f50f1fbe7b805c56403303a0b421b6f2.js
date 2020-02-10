58ca2abea8bbc9f9b5733aac202eea1225e0fe41
Tyler Knapp <Knapptr@gmail.com>
2020-02-10 12:47:41 -0800

    Updates

---
+++ precache-manifest.f50f1fbe7b805c56403303a0b421b6f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbf3a3549fee49efa392204f154f5ac",
+    "url": "/weatherCard/index.html"
+  },
+  {
+    "revision": "015be8c37e6ca8617198",
+    "url": "/weatherCard/static/js/2.c4a648b4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weatherCard/static/js/2.c4a648b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80cd3d0a8059354564e7",
+    "url": "/weatherCard/static/js/main.7c509a60.chunk.js"
+  },
+  {
+    "revision": "77d5274b565344c052ee",
+    "url": "/weatherCard/static/js/runtime-main.022d3bff.js"
+  }
+]);
